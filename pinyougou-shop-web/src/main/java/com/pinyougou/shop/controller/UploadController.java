39f5ebf45bd2ca8999dd79617467e3a9dded9c9d
misterWei <mynameisweiyan@gmail.com>
2018-12-05 10:15:17 +0800

    File _System

--- pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/UploadController.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/UploadController.java
@@ -1,5 +1,10 @@
 package com.pinyougou.shop.controller;
 
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.StorageServer;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +26,16 @@ public class UploadController {
 	public CRUDResult upload(MultipartFile file){
 		
 		try{
+			//fastDFS文件下载功能完善   ------
+			ClientGlobal.init("classpath:/config/fdfs_client.conf");
+			 TrackerClient tracker = new TrackerClient(); 
+	            TrackerServer trackerServer = tracker.getConnection(); 
+	            StorageServer storageServer = null;
+
+	            StorageClient storageClient = new StorageClient(trackerServer, storageServer); 
+			
+			 byte[] arr =  storageClient.download_file("", "");
+			 //--------
 		//获取文件的扩展名称:
 		String filename = file.getOriginalFilename();
 		//需要指定后缀名称 MIME类型
@@ -30,6 +45,7 @@ public class UploadController {
 		FastDFSClient client = new FastDFSClient("classpath:/config/fdfs_client.conf");
 		//           传入图片字节进制信息,并将后缀名传入
 		String uploadFile = client.uploadFile(file.getBytes(),UrlMime);
+		
 		//返回一个全路径带file_id的数据 我们需要将URI和file_id进行拼接
 		String Url = FILE_SERVER_URL+uploadFile;
 		//返回页面一个url地址
